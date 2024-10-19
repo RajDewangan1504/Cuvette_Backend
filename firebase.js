664bfe291eaca187bb2c22825fe730b7c69414e0
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-10-19 17:03:51 +0530

    done

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
 // firebase.js
+const path = require('path');
 const admin = require('firebase-admin');
 require('dotenv').config(); // Make sure to load environment variables
 
 // Resolve the relative path to an absolute path
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccountPath = path.resolve(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
